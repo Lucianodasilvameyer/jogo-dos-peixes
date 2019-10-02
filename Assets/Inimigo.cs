915c5b20f3b21f0e94ac1bddcdb3c0ac9927aea6
Lucianodasilvameyer <murcilhax@hotmail.com>
2019-10-02 16:10:42 -0300

    trabalhar na invenciblilidade

---
+++ Assets/Inimigo.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Inimigo : MonoBehaviour
+{
+    public AudioSource audioSource;
+    public AudioClip SomDeColisao;
+
+    [SerializeField]
+    private Transform target;
+
+    [SerializeField]
+    float speed;
+
+    [SerializeField]
+    private Vector2 direction;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        DefinirAlvo();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Mover();
+    }
+    public void Mover()
+    {
+        transform.Translate(speed* direction* Time.deltaTime);
+    }
+    
+
+
+
+    public void DefinirAlvo()
+    {
+        if(!target || target == null)
+        {
+            target = GameObject.FindGameObjectWithTag("Peixe").transform;
+
+            direction = target.position - transform.position;
+            direction = direction.normalized;
+        }
+    }
+    public void somPlay(AudioClip som)
+    {
+        audioSource.clip = som;
+        audioSource.Play();
+    }
+}
---
+++ Assets/Player.cs
@@ -0,0 +1,75 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+public class Player : MonoBehaviour
+{
+
+    Rigidbody2D righ;
+    public float speed;
+
+    
+
+    bool isDead = false;
+
+    [SerializeField]
+    private int vidaMax;
+
+    [SerializeField]
+    private int vidaInicial;
+
+    public Slider slider_;
+
+    [SerializeField]
+    private int vida;
+
+    public int Vida
+    {
+        get
+        {
+            return Vida;
+        }
+        set
+        {
+            if (value <= 0)
+            {
+                vida = 0;
+            }
+            else if(value> vidaMax)
+            {
+                vida = vidaMax;
+            }
+            else
+            {
+                value = vida;
+                slider_.value = (float)vida / (float)vidaMax;
+
+            }
+                
+        }
+    }
+
+
+   
+    
+
+
+    // Start is called before the first frame update
+    private void FixedUpdate()
+    {
+        Vector3 Position = new Vector3(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical"));
+        righ.velocity = Position * speed;
+    }
+
+    void Start()
+    {
+        Vida = vidaInicial;
+        righ = GetComponent<Rigidbody2D>();
+        
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
