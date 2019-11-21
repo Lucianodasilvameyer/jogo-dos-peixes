6049c653bb8d9e1077702345b0e6467991f37068
Lucianodasilvameyer <51789104+Lucianodasilvameyer@users.noreply.github.com>
2019-11-21 11:50:10 -0300

    ainda a camera

--- Assets/scripts/CameraMovement.cs
+++ Assets/scripts/CameraMovement.cs
@@ -19,8 +19,13 @@ public class CameraMovement : MonoBehaviour
     {
         if(transform.position != target.position)
         {
-            Vector3 targetposition = new Vector3(target.position.x, target.position.y, transform.position.z);
-            transform.position = Vector3.Lerp(transform.position,targetposition,smoothing);
+            Vector3 targetPosition = new Vector3(target.position.x, target.position.y, transform.position.z);
+
+            targetPosition.x = Mathf.Clamp(targetPosition.x, minPosition.x, maxPosition.x);
+
+            targetPosition.y = Mathf.Clamp(targetPosition.y, minPosition.y, maxPosition.y);
+
+            transform.position = Vector3.Lerp(transform.position,targetPosition,smoothing);
             
         }
     }
